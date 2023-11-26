33a6ab25bae1a7f5cef6e3bed47977b698db3afa
Prajwal Bhandari <bhandaripraj7@gmail.com>
2023-11-26 22:50:40 +0200

    can get accessed to signup form fields

--- client/src/SignupView.js
+++ client/src/SignupView.js
@@ -2,11 +2,34 @@ import React from "react"
 
 
 export default function SignupView() {
-    return (
+    const handleSignupSubmit= (event) => {
+        event.preventDefault();
+        console.log(event.target.username.value);
+        console.log(event.target.email.value);
+        console.log(event.target.password.value);
+    }
+            return (
         <div>
-           This is the signup view
+        <h2>Sign up</h2>
+        <form onSubmit={handleSignupSubmit}>
+            <div>
+                Username <br/>
+                <input type="text" name="username"></input>
         </div>
+        <div>
+                Email <br/>
+                <input type="text" name="email"></input>
+        </div>
+        <div>
+                Password <br/>
+                <input type="text" name="password"></input>
+        </div>
+        <div>
+                <button type="submit" >Sign up</button>
+        </div>
+        </form>
+        </div>
+        
     )
-
     
 }
