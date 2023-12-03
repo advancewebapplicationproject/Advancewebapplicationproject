d24d210c2b34570198296249eb8b1ff107121205
Prajwal Bhandari <bhandaripraj7@gmail.com>
2023-12-03 22:35:12 +0200

    protected view completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,92 +1,44 @@
-import React from 'react'
-import './App.css'
-import Home from './Home'
-import LoginView from './LoginView'
-import SignupView from './SignupView'
-import ProtectedView from './ProtectedView'
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import { useState} from 'react';
+import React from 'react';
+import './App.css';
+import Home from './Home';
+import LoginView from './LoginView';
+import SignupView from './SignupView';
+import ProtectedView from './ProtectedView';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
 function App() {
   const [userJwt, setUserJwt] = useState(null);
-  
+
   let authRoutes = (
     <>
-   <Route path='/login' element= {<LoginView login={ newJwt => 
-    setUserJwt(newJwt) } /> } />
-        <Route path='/signup' element= { <SignupView /> } />
-  </>
+      <Route path='/login' element={<LoginView login={(newJwt) => {
+        setUserJwt(newJwt);
+        window.localStorage.setItem('appAuthData', newJwt);
+      }} />} />
+      <Route path='/signup' element={<SignupView />} />
+    </>
   );
 
   if (userJwt != null) {
-    authRoutes = <Route path='/protected' element= { <ProtectedView jwt={userJwt}/> }
-    />
+    authRoutes = <Route path='/protected' element={<ProtectedView jwt={userJwt} logout={() => {
+      setUserJwt(null);
+      window.localStorage.removeItem('appAuthData');
+    }} />} />;
   }
+
   return (
     <div>
       <h1>React auth demo</h1>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element= { <Home userLoggedIn={userJwt != null} /> }  />
-        {authRoutes}
-       
-        <Route path="*" element= { <Home userLoggedIn={userJwt != null} /> } />
-      
-      </Routes>
-     </BrowserRouter>
-
-    </div>
-  )
-}
-
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*  function App() {
-  const [backendData, setBackendData] = useState([]);
-
-  useEffect(() => {
-    fetch("http://localhost:5005/my-protected-resource", {
-  method: 'GET',
-  headers: {
-    'Authorization': 'Basic ' + btoa('userOne:passwordOne')
-  }
-})
-.then(response => response.text())
-.then(data => console.log(data))
-.catch(error => console.error('Error:', error));
-  } ,
-
-       []);
-
-  return (
-    <div>
-      {backendData.users ? (
-        backendData.users.map((user, i) => <p key={i}>{user}</p>)
-      ) : (
-        <p>Loading.....</p>
-      )}
+        <Routes>
+          <Route path='/' element={<Home userLoggedIn={userJwt != null} />} />
+          {authRoutes}
+          <Route path="*" element={<Home userLoggedIn={userJwt != null} />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
-*/
-
--- client/src/ProtectedView.js
+++ client/src/ProtectedView.js
@@ -1,20 +1,60 @@
-import React from "react"
-import jwt from 'jsonwebtoken'
-
-
+import React, { useState } from "react";
+import jwt from 'jsonwebtoken';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import constants from './Constants.json'; // Import the default export
 
 export default function ProtectedView(props) {
-    const decodedJWT = jwt.decode(props.jwt);
-    
-    console.log(decodedJWT);
-    return (
-        <div class="protected">
-                <h2>protected view</h2>
-                <div>
-                    Decoded JWT data from payload : <br/>
+  const [userTodos, setUserTodos] = useState([]);
 
-                </div>
-           
+  const decodedToken = jwt.decode(props.jwt);
+
+  console.log(decodedToken);
+
+  const loadDataWithJWT = async () => {
+    try {
+      const results = await axios.get(constants.API_ADDRESS + '/todosJWT', {
+        headers: {
+          Authorization: 'Bearer ' + props.jwt
+        }
+      });
+      console.log("results", results.data);
+      setUserTodos(results.data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return (
+    <div className="protected">
+      <h2>protected view</h2>
+      <div>
+        Decoded JWT data from payload: <br />
+        User Id: {decodedToken.user.id} <br />
+        User email: {decodedToken.user.email} <br />
+      </div>
+      <div>
+        <button onClick={loadDataWithJWT}>Click to load user todos with JWT</button>
+      </div>
+      <div>
+        <table>
+          <tbody>
+            {userTodos.map(t => (
+              <tr key={t.id}>
+                <td>{t.description}</td>
+                <td>{t.dueDate}</td>
+                <td>{t.statue}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div>
+        <Link to="/">Go back to home view</Link>
+        <div>
+          <button onClick={props.logout}>Logout</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
