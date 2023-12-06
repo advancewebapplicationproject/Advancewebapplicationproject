d17332d6a3e00bc7a6846ef27738642b9cda0eac
Prajwal Bhandari <bhandaripraj7@gmail.com>
2023-12-06 15:33:52 +0200

    added some infos in home view

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,23 +1,41 @@
-import React from "react"
-import { Link } from "react-router-dom"
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css"; // Import a separate CSS file for styling
 
 export default function Home(props) {
-    return (
+  return (
+    <div className="home-container">
+      {/* Header */}
+      <header>
+        <h1>MyParcels App</h1>
+      </header>
+
+      {/* Main Content */}
+      <div className="main-content">
         <div>
-             This is the home view
-        <div>
-            user login status : {props.userLoggedIn ? "is logged in" :  "not logged in"  } 
-            </div>
-            <div>
-                {props.userLoggedIn ? <Link to = "protected">Go to protected view</Link> : <>
-                <Link to = "signup">Sign up</Link><br/>
-        <Link to = "login">Login</Link><br/>
-                </>
-                }
-        
-              
-            </div>
+          <p>Welcome to MyParcels, your go-to parcel management app!</p>
+          <p>Discover how easy it is to manage your parcels with us.</p>
         </div>
-    )
+
+        <div>
+          <p>User login status: {props.userLoggedIn ? "is logged in" : "not logged in"}</p>
+          {props.userLoggedIn ? (
+            <Link to="protected">Go to protected view</Link>
+          ) : (
+            <>
+              <Link to="signup">Sign up</Link>
+              <br />
+              <Link to="login">Login</Link>
+              <br />
+            </>
+          )}
+        </div>
+      </div>
+
+      {/* Footer */}
+      <footer>
+        <p>&copy; 2023 MyParcels App. All rights reserved.</p>
+      </footer>
+    </div>
+  );
 }
