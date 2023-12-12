e4822223f18dbb4597f7943bc9b73083b0e13082
Prajwal Bhandari <bhandaripraj7@gmail.com>
2023-12-12 21:29:24 +0200

    colon changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,72 @@
-import React from 'react';
-import './App.css';
-import Home from './Home';
-import AboutUs from './AboutUs';
-import LoginView from './LoginView';
-import SignupView from './SignupView';
-import ProtectedView from './ProtectedView';
-import BoxDetails from './BoxOptions';
-import ReceiverForm from './ReceiverForm';
-import StaffForm from './StaffForm';
-import Sender from './Sender';
-import SenderForm from './SenderForm';   
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
+import React from "react";
+import "./App.css";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import LoginView from "./LoginView";
+import SignupView from "./SignupView";
+import ProtectedView from "./ProtectedView";
+import BoxDetails from "./BoxOptions";
+import ReceiverForm from "./ReceiverForm";
+import StaffForm from "./StaffForm";
+import Sender from "./Sender";
+import SenderForm from "./SenderForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
-const jwtFromStorage = window.localStorage.getItem('appAuthData');
+const jwtFromStorage = window.localStorage.getItem("appAuthData");
 
 function App() {
   const [userJwt, setUserJwt] = useState(jwtFromStorage);
- 
+
   let authRoutes = (
     <>
       <Route
-  path='/login'
-  element={
-    <LoginView
-      login={ newJwt => {
-        setUserJwt(newJwt)
-        window.localStorage.setItem('appAuthData', newJwt);
-      }}
-    /> 
-  }
-/>
-      <Route path='/signup' element={<SignupView />} />
+        path="/login"
+        element={
+          <LoginView
+            login={(newJwt) => {
+              setUserJwt(newJwt);
+              window.localStorage.setItem("appAuthData", newJwt);
+            }}
+          />
+        }
+      />
+      <Route path="/signup" element={<SignupView />} />
     </>
   );
 
   if (userJwt != null) {
-    authRoutes = <Route path='/protected' element={<ProtectedView jwt={userJwt} logout={() => {
-      setUserJwt(null);
-      window.localStorage.removeItem('appAuthData');
-    }} />} />;
+    authRoutes = (
+      <Route
+        path="/protected"
+        element={
+          <ProtectedView
+            jwt={userJwt}
+            logout={() => {
+              setUserJwt(null);
+              window.localStorage.removeItem("appAuthData");
+            }}
+          />
+        }
+      />
+    );
   }
 
   return (
     <div>
-      <h1>Parcel Tracker</h1> 
+      <h1>Parcel Tracker</h1>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home userLoggedIn={userJwt != null} />} />
-          <Route path='/about' element={<AboutUs />} />
+          <Route path="/" element={<Home userLoggedIn={userJwt != null} />} />
+          <Route path="/about" element={<AboutUs />} />
           {authRoutes}
           <Route path="*" element={<Home userLoggedIn={userJwt != null} />} />
-          <Route path='/sender' element={<Sender />} />
-          <Route path='/sender-form' element={<SenderForm />} />
-          <Route path='/box-options' element={<BoxDetails />} />
-          <Route path='/receiver' element={<ReceiverForm />} />
-          <Route path='/staff' element={<StaffForm />} />
-          <Route path='/box-details/:boxIndex' element={<BoxDetails />} />
+          <Route path="/sender" element={<Sender />} />
+          <Route path="/sender-form" element={<SenderForm />} />
+          <Route path="/box-options" element={<BoxDetails />} />
+          <Route path="/receiver" element={<ReceiverForm />} />
+          <Route path="/staff" element={<StaffForm />} />
+          <Route path="/box-details/:boxIndex" element={<BoxDetails />} />
         </Routes>
       </BrowserRouter>
     </div>
